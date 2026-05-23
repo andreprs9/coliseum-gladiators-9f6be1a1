7f9675adb9d3fbbccca2c8c5c0e6a70d5b670a28
A.Pires <andrepires_ciber@outlook.com>
2026-05-23 19:25:40 -0300

    Delete api directory

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { createStartHandler } from '@tanstack/react-start/server'
-import { getRouterManifest } from '@tanstack/react-start/router-manifest'
-
-export default createStartHandler({
-  createRouter: () => import('../src/router').then(m => m.createRouter()),
-  getRouterManifest: () => getRouterManifest(),
-})
